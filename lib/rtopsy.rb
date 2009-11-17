83b0d96d62bf0762dc1d6c3842689bc50c843d0b
Ernesto Tagwerker <etagwerker@aycron.com>
2009-11-17 17:01:31 -0500

    Trying to solve dependency problem.

--- lib/rtopsy.rb
+++ lib/rtopsy.rb
@@ -1,6 +1,5 @@
 require 'rubygems'
 require 'json'
-require 'HTTParty'
 
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
